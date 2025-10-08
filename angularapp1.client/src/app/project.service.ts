6ec4cd699bab75cb21d5573a5b70f5229cabc226
Mostafa Said <m.ssaid356@gmail.com>
2025-10-08 11:52:36 +0300

    last done

---
+++ angularapp1.client/src/app/project.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
+import { environment } from '../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class ProjectService {
+  private apiUrl = `${environment.apiBaseUrl}/api/projects`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.apiUrl);
+  }
+}
